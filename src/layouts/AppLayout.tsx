135187112e2ad891ca8ec96fa34bd413e3e6a11e
Franklin-Gomez <franklin_043097@hotmail.com>
2024-08-30 11:09:02 -0500

    working on principal layout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,15 +1,38 @@
 import { Outlet } from "react-router-dom";
+import Logo from "../components/Logo";
 
 export default function AppLayout() {
     return (
 
         <>
 
-            <div>
-                desde AppLayout
-            </div>
+            <header className="bg-gray-800 py-5">
+                <div className=" container max-w-screen-xl mx-auto flex flex-col sm:flex-row justify-between items-center">
+                    
+                    <div className=" w-64">
+                        
+                        <Logo/>
 
-            <Outlet/>
+                    </div>
+
+                    <div>
+
+                    </div>
+                </div>
+            </header>
+
+
+            <section className=" container max-w-screen-xl mx-auto mt-10 p-5">
+                
+                <Outlet/>
+
+            </section>
+
+            <footer className="py-5">
+                <p className="text-center">
+                    Todos los derechos reservados { new Date().getFullYear() }
+                </p>
+            </footer>
         
         </>
 

ff4c07f363c0805eced7080267c151774b2dc6cc
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-04 09:44:01 -0500

    add reactQueryDevTools in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import Router from './router'
 
 import { QueryClient , QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools/production'
 
 const queryClient = new QueryClient()
 
@@ -14,6 +15,8 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
 
       <Router/>
+
+      <ReactQueryDevtools/>
 
     </QueryClientProvider>
 

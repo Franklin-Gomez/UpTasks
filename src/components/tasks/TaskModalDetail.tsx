ad36dd132ac29865fb18999d938b1224000da81d
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-09 10:33:46 -0500

    change format to date

--- src/components/tasks/TaskModalDetail.tsx
+++ src/components/tasks/TaskModalDetail.tsx
@@ -6,6 +6,8 @@ import { useQuery } from '@tanstack/react-query';
 import { getTaskById } from '@/api/TaskAPI';
 import { toast } from 'react-toastify';
 
+import { formatDate } from '@/utils/utils';
+
 export default function TaskModalDetails() {
 
     const params = useParams()
@@ -62,7 +64,7 @@ export default function TaskModalDetails() {
                                 leaveTo="opacity-0 scale-95"
                             >
                                 <Dialog.Panel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
-                                    <p className='text-sm text-slate-400'>Agregada el: </p>
+                                    <p className='text-sm text-slate-400'>Agregada el: { formatDate( data .createdAt)}</p>
                                     <p className='text-sm text-slate-400'>Última actualización: </p>
                                     
                                     <Dialog.Title

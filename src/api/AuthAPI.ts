b80cf4a3e07a9a7f0249b3224cf986fb0320f298
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-12 14:06:02 -0500

    create user from register form

---
+++ src/api/AuthAPI.ts
@@ -0,0 +1,20 @@
+import api from "@/lib/axios";
+import { isAxiosError } from "axios";
+import { UserRegistrationForm } from "../types";
+
+export const createAccount  = async ( formData : UserRegistrationForm) => { 
+
+    try {
+        const url = '/auth/create-account'
+        
+        const { data } = await api.post<string>( url , formData ) 
+
+        return data 
+        
+    } catch (error) {
+        if( isAxiosError(error) && error.response  ) { 
+            throw new Error( error.response.data.error )
+        }
+    }
+
+}

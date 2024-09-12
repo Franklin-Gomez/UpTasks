a465c08137b0483358db92e66123c02e32c14b4c
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-12 10:49:43 -0500

    create layour principal and routes

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,8 @@ import DashboardView from './views/DashboardView'
 import CreateProjectView from './views/projects/CreateProjectView'
 import EditProjectView from './views/projects/EditProjectView'
 import ProjectDetailsViews from './views/projects/ProjectDetailsViews'
+import AuthLayout from './layouts/AuthLayout'
+import LoginView from './views/auth/LoginView'
 
 export default function Router() {
 
@@ -17,6 +19,11 @@ export default function Router() {
                     <Route path='/projects/:projectId' element={<ProjectDetailsViews/>} />
                     <Route path='/projects/:projectId/edit' element={<EditProjectView/>} />
 
+                </Route>
+
+                // auth
+                <Route element={<AuthLayout/> }>
+                    <Route index path='/auth/login' element={<LoginView/>}/>
                 </Route>
             </Routes>
         

f3fb6298a1d92397614f9561b332291cf765d2ba
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-16 10:36:21 -0500

    create comunication with backend to  send email to recover password

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { confirmToken, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
+import { confirmToken, ForgotPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
 
 export const createAccount  = async ( formData : UserRegistrationForm) => { 
 
@@ -56,6 +56,23 @@ export const AuthenticateUser  = async ( formData  : UserLoginForm) => {
 
     try {
         const url = '/auth/login'
+        
+        const { data } = await api.post<string>( url , formData  ) 
+
+        return data 
+        
+    } catch (error) {
+        if( isAxiosError(error) && error.response  ) { 
+            throw new Error( error.response.data.error )
+        }
+    }
+
+}
+
+export const forgotPassword  = async ( formData  : ForgotPasswordForm) => { 
+
+    try {
+        const url = '/auth/forgot-password'
         
         const { data } = await api.post<string>( url , formData  ) 
 

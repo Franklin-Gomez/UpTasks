b90fdb38936a87d1ec356eca33f3158297b554e0
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-16 11:24:47 -0500

    create newPassword view route , first show newPasswordOtken and second show NewPasswordForm

--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -1,8 +1,30 @@
+import NewPasswordToken from "@/components/auth/NewPasswordToken";
+import { useState } from "react";
+import NewPasswordForm from "@/components/auth/NewPasswordForm";
 
 export default function NewPasswordView() {
+
+  const [ isValidToken , serIsValidToken ] = useState( false )
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1 className="text-5xl font-black text-white">Reestablecer Password</h1>
+      <p className="text-2xl font-light text-white mt-5">
+        Ingresa el codigo que recibistes  {''}
+        <span className=" text-fuchsia-500 font-bold"> en el Email </span>
+      </p>
+
+      { !isValidToken ? 
+        // componente para ingresar el token 
+        <NewPasswordToken/> 
+        
+        : 
+        
+        // componente para ingresar la nueva contraseña
+        <NewPasswordForm/> 
+      }
+
+
+    </>
   )
 }

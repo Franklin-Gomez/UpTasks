05136738204094f6f28d4d01a585201c986c7177
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-25 09:16:46 -0500

    change taskschema for show completeBy

--- src/types/index.ts
+++ src/types/index.ts
@@ -43,8 +43,10 @@ export const taskSchema = z.object({
     description : z.string(),
     project : z.string(),
     status : taskStatusSchema,
+    completeBy : userSchema.or(z.null()) ,
     createdAt : z.string(),
     updatedAt : z.string()
+    
 })
 
 export type TaskType = z.infer<typeof taskSchema>

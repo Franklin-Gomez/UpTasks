f5fdc0336ea33a786546c525917e682314c1e655
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-26 11:52:32 -0500

    create componentent to show note form

--- src/components/tasks/TaskModalDetail.tsx
+++ src/components/tasks/TaskModalDetail.tsx
@@ -9,6 +9,7 @@ import { toast } from 'react-toastify';
 import { formatDate } from '@/utils/utils';
 import { statusTranslations } from '@/locales/es';
 import { taskStatusType } from '@/types/index';
+import NotesPanel from '../notes/NotesPanel';
 
 
 
@@ -99,22 +100,29 @@ export default function TaskModalDetails() {
 
                                     <p className='text-lg text-slate-500 mb-2'>Descripción: {data.description }</p>
 
-                                    <p className='text-2xl text-slate-500 mb-2'>Historial de Cambios</p>
+                                    { data.completedBy.length ? ( 
 
-                                    <ul className='list-decimal'>
-                                        { data.completedBy.map( ( activityLog )  => (
+                                        <>
+                                            <p className='text-2xl text-slate-500 mb-2'>Historial de Cambios</p>
 
-                                            <li key={activityLog._id}>
+                                            <ul className='list-decimal'>
+                                                { data.completedBy.map( ( activityLog )  => (
 
-                                                <span className='font-bold text-slate-600'> { statusTranslations[activityLog.status] } : </span> {' '} 
+                                                    <li key={activityLog._id}>
+
+                                                        <span className='font-bold text-slate-600'> { statusTranslations[activityLog.status] } : </span> {' '} 
+                                                        
+                                                        por : { activityLog.user.name }
+
+                                                    </li>
                                                 
-                                                por : { activityLog.user.name }
+                                                ))}
 
-                                            </li>
-                                        
-                                        ))}
+                                            </ul>  
+                                        </>
 
-                                    </ul>
+                                    ) : null }
+
 
                                     <div className='my-5 space-y-3'>
                                         <label className='font-bold'>Estado Actual:</label>
@@ -131,6 +139,8 @@ export default function TaskModalDetails() {
                                         </select>
 
                                     </div>
+
+                                    <NotesPanel/>
                                     
                                 </Dialog.Panel>
                             </Transition.Child>

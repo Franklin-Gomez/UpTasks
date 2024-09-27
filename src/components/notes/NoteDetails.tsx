d994493a3f04834e12b69a58b2752f5bedc9fd2f
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-27 11:02:16 -0500

    you create the note? if yes, you can delete the note

--- src/components/notes/NoteDetails.tsx
+++ src/components/notes/NoteDetails.tsx
@@ -1,11 +1,19 @@
+import { useAuth } from "@/hooks/useAuth"
 import { Note } from "@/types/index"
 import { formatDate } from "@/utils/utils"
+import { useMemo } from "react"
 
 type NoteDetailsProps = { 
     note : Note
 }
 
 export default function NoteDetails({ note } : NoteDetailsProps ) {
+
+    const { data , isLoading } = useAuth()
+    const canDelete = useMemo(() => data?._id === note.createdBy._id , [ data ])
+
+    if( isLoading ) return 'Cargando...'
+
     return (
         <div className="p-3 flex justify-between items-center">
             <p>
@@ -15,6 +23,15 @@ export default function NoteDetails({ note } : NoteDetailsProps ) {
             <p className="text-xs text-slate-500">
                 { formatDate( note.createdAt )}
             </p>
+            
+            { canDelete && ( 
+                <button
+                    type="button"
+                    className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor pointer transition-colors"
+                > Eliminar</button>
+            )}
+
         </div>
+
     )
 }

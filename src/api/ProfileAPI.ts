3a6af89745a58012190a155f354ea1071fec2308
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-30 11:53:02 -0500

    change password for client

--- src/api/ProfileAPI.ts
+++ src/api/ProfileAPI.ts
@@ -1,5 +1,5 @@
 import { isAxiosError } from "axios"
-import { UserProfileForm } from "../types"
+import { UpdateCurrentPasswordForm, UserProfileForm } from "../types"
 import api from "@/lib/axios"
 
 export const updateProfile = async ( formData : UserProfileForm ) => {
@@ -7,6 +7,24 @@ export const updateProfile = async ( formData : UserProfileForm ) => {
     try {
 
         const { data } = await api.put<string>('/auth/profile' , formData )
+
+        return data
+
+                
+    } catch (error) {
+
+        if( isAxiosError( error) && error.response ) { 
+            throw new Error(error.response.data.error)
+        }
+
+    }
+}
+
+export const changePassword = async ( formData : UpdateCurrentPasswordForm ) => {
+     
+    try {
+
+        const { data } = await api.post<string>('/auth/update-password' , formData )
 
         return data
 

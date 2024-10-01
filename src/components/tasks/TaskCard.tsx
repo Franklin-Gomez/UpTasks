6e24b7525423b598bf03a0ecc92c471b59e26bc6
Franklin-Gomez <franklin_043097@hotmail.com>
2024-10-01 14:04:00 -0500

    config useDraggable

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -6,6 +6,7 @@ import { useNavigate, useParams } from "react-router-dom"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { deleteTask } from "@/api/TaskAPI"
 import { toast } from "react-toastify"
+import { useDraggable } from "@dnd-kit/core"
 
 type TaskCardProps = { 
     task : TaskType
@@ -13,14 +14,16 @@ type TaskCardProps = {
 }
 
 export default function TaskCard( { task , canEdit } : TaskCardProps) {
+    
+    const { attributes , listeners , setNodeRef , transform } = useDraggable({
+        id : task._id
+    })
 
     const navigate = useNavigate()
-
     const params = useParams()
     const projectId = params.projectId!
 
     const queryclient = useQueryClient()
-
     const { mutate } = useMutation({ 
         mutationFn : deleteTask,
         onError : ( error ) => {
@@ -32,11 +35,21 @@ export default function TaskCard( { task , canEdit } : TaskCardProps) {
         }
     })
 
+    const style = transform ? { 
+
+    } : undefined
+
 
     return (
         <>
             <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
-                <div className="min-w-0 flex flex-col gap-y-4 ">
+                <div 
+                    // pasandole las funcionalidades listeners y attributes
+                    {...listeners}
+                    {...attributes}
+                    ref={setNodeRef}
+                    style={style}
+                    className="min-w-0 flex flex-col gap-y-4 ">
                     <button
                         type="button"
                         className="text-xl font-bold text-slate-600 text-left"

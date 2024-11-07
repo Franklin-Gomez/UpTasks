786bbc44f19fe082d3b27ea7bb360d41900915bb
Franklin-Gomez <franklin_043097@hotmail.com>
2024-11-07 11:35:19 -0500

    create layouts dashboard

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,11 +1,49 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
+import Logo from "../components/Logo"
+import NavMenu from "../components/NavMenu"
 
 export default function AppLayout() {
     return (
-        <div>
-            Desde App Layout
 
-            <Outlet/>
+        <div>
+
+            <header className="py-5 bg-gray-800">
+
+                <div className="flex justify-between items-center  container mx-auto align-middle">
+
+                    <div className="w-64">
+
+                        <Link
+                            to={'/'}
+                        >
+                            <Logo/>
+
+                        </Link>
+
+                    </div>
+
+                    <NavMenu
+                        name="D.Gomez"
+                    />
+
+                </div>
+                
+
+            </header>
+
+
+            <section className="py-5 mx-auto container px-20">
+
+                <Outlet/>
+
+            </section>
+
+            <footer className="py-5 ">
+                <p className="text-center"> Todos los Derechos reservados </p>
+            </footer>
+
+
         </div>
+
     )
 }
